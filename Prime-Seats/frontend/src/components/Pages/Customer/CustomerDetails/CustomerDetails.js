9e041ae9289833369d967396abf1ebd186ebfdb2
Gagan <gagan.vc@xcdify.com>
2024-03-13 09:31:47 +0530

    fixed the ticket download

--- Prime-Seats/frontend/src/components/Pages/Customer/CustomerDetails/CustomerDetails.js
+++ Prime-Seats/frontend/src/components/Pages/Customer/CustomerDetails/CustomerDetails.js
@@ -1,57 +1,76 @@
-// import React, { useEffect, useState } from 'react'
-// import { IoPersonSharp } from "react-icons/io5";
-// import "./CustomerDetails.css"
-// import { MdDownload } from "react-icons/md";
+// import React, { useEffect, useState } from 'react';
+// import { IoPersonSharp } from 'react-icons/io5';
+// import './CustomerDetails.css';
+// import { MdDownload } from 'react-icons/md';
 // import axios from 'axios';
+// import jsPDF from 'jspdf';
+// import { Link } from 'react-router-dom';
 
 // const CustomerDetails = () => {
-//     const [customer, setcustomer] = useState([])
-//     const [booking, setBooking] = useState([])
+//     const [customer, setCustomer] = useState([]);
+//     const [booking, setBooking] = useState([]);
 
-
-//     const getcustomerById = async () => {
-//         const id = sessionStorage.getItem("customerId");
+//     const getCustomerById = async () => {
+//         const id = sessionStorage.getItem('customerId');
 //         const res = await axios.get(`http://localhost:5000/customer/${id}`).catch((err) => console.log(err));
 //         if (res.status !== 200) {
-//             return console.log("Unexpected Error");
+//             return console.log('Unexpected Error');
 //         }
 //         const resData = await res.data;
 //         return resData;
 //     };
 
-//     const getcustomerBooking = async () => {
-//         const id = sessionStorage.getItem("customerId");
-//         const res = await axios
-//             .get(`http://localhost:5000/booking/${id}`)
-//             .catch((err) => console.log(err));
+//     const getCustomerBooking = async () => {
+//         const id = sessionStorage.getItem('customerId');
+//         const res = await axios.get(`http://localhost:5000/booking/${id}`).catch((err) => console.log(err));
 
 //         if (res.status !== 200) {
-//             return console.log("Unexpected error");
+//             return console.log('Unexpected error');
 //         }
 //         const resData = await res.data;
-//         // console.log(resData)
 //         return resData;
-//     }
+//     };
 
-//     // const deleteBooking = async (id) => {
-//     //     const res = await axios
-//     //         .delete(`http://localhost:5000/booking/${id}`)
-//     //         .catch((err) => console.log(err));
+//     const generatePDFContent = () => {
+//         const doc = new jsPDF();
+//         doc.setFontSize(22);
+//         doc.setFont('helvetica', 'bold');
+//         doc.text(`Name: ${customer.first_name} ${customer.last_name}`, 10, 20);
+//         doc.setFontSize(16);
+//         doc.setFont('helvetica', 'normal');
+//         doc.text(`Email: ${customer.email}`, 10, 30);
+//         doc.text(`Contact Number: ${customer.contact_number}`, 10, 40);
+//         doc.setFontSize(18);
+//         doc.setFont('helvetica', 'bold');
+//         doc.text('Bookings:', 10, 60);
+//         let y = 70;
+//         booking.filter((data) => !data.is_deleted).forEach((data) => {
+//             doc.setFontSize(14);
+//             doc.setFont('helvetica', 'normal');
+//             doc.text(`Movie: ${data.movieId.name}`, 10, y);
+//             doc.text(`Theater: ${data.theaterid.name}`, 10, y + 10);
+//             doc.text(`Seats: ${data.seats.join(', ')}`, 10, y + 20);
+//             doc.text(`Date: ${data.date}`, 10, y + 30);
+//             y += 50;
+//         });
+//         return doc;
+//     };
 
-//     //     if (res.status !== 200) {
-//     //         return console.log("Unepxected Error");
-//     //     }
 
-//     //     const resData = await res.data;
-//     //     getcustomerBooking().then(data => setBooking(data.book)).catch(err => console.log(err));
-//     //     return resData;
-//     // }
+
+//     const downloadPDF = () => {
+//         const doc = generatePDFContent();
+//         doc.save('booking_details.pdf');
+//     };
 
 //     useEffect(() => {
-//         getcustomerById().then(data => setcustomer(data.customer)).catch(err => console.log(err));
-//         getcustomerBooking().then(data => setBooking(data.book)).catch(err => console.log(err));
-
-//     }, [])
+//         getCustomerById()
+//             .then((data) => setCustomer(data.customer))
+//             .catch((err) => console.log(err));
+//         getCustomerBooking()
+//             .then((data) => setBooking(data.book))
+//             .catch((err) => console.log(err));
+//     }, []);
 
 //     return (
 //         <div className='customerdetails-box'>
@@ -60,15 +79,18 @@
 //             </div>
 //             <div className='customerdetails-whitebox'>
 //                 <div className='customerdetails-details'>
-//                     <div className='admindetails-title'><span>YOUR DETAILS</span>
-//                         <span className='admindetails-underline' ></span>
+//                     <div className='admindetails-title'>
+//                         <span>YOUR DETAILS</span>
+//                         <span className='admindetails-underline'></span>
 //                     </div>
 //                     <div className='customerdetails-persondetails'>
-//                         <h1 style={{ color: "#1e3249" }}><span>Name: {customer.first_name} {customer.last_name}</span></h1>
-//                         <h1 style={{ color: "#1e3249" }}> <span>Email: {customer.email}</span></h1>
-//                         <h1 style={{ color: "#1e3249" }}> <span>Contact Number: {customer.contact_number}</span></h1>
+//                         <h1 style={{ color: '#1e3249' }}>
+//                             <span>Name: {customer.first_name} {customer.last_name}</span>
+//                         </h1>
+//                         <h1 style={{ color: '#1e3249' }}> <span>Email: {customer.email}</span></h1>
+//                         <h1 style={{ color: '#1e3249' }}> <span>Contact Number: {customer.contact_number}</span></h1>
 //                     </div>
-//                     {(booking.some((data) => data.is_deleted == false)) ?
+//                     {booking.some((data) => !data.is_deleted) ? (
 //                         <div className='table-container'>
 //                             <table>
 //                                 <thead>
@@ -81,39 +103,37 @@
 //                                     </tr>
 //                                 </thead>
 //                                 <tbody>
-//                                     {booking.map((data, index) =>
-//                                     (
+//                                     {booking.map((data, index) => (
 //                                         data.is_deleted === false && (
 //                                             <tr key={index}>
 //                                                 <td>{data.movieId.name}</td>
-//                                                 <td>{data.theaterid.name}</td>
+//                                                 <td><a href={data.theaterid.location} target="_blank">{data.theaterid.name}</a></td>
 //                                                 <td>{data.seats.join(', ')}</td>
 //                                                 <td>{data.date}</td>
-//                                                 <td><div className='customerdetails-delete-icon' style={{ color: "#1e3249" }}><MdDownload /></div></td>
+
+//                                                 <td>
+//                                                     <div className='customerdetails-delete-icon' style={{ color: '#1e3249', cursor: 'pointer' }} onClick={downloadPDF}>
+//                                                         <MdDownload />
+//                                                     </div>
+//                                                 </td>
 //                                             </tr>
 //                                         )
-//                                     )
-
-//                                     )}
+//                                     ))}
 //                                 </tbody>
 //                             </table>
 //                         </div>
-
-
-//                         : (
-
-//                             <div>No bookings found</div>
-//                         )}
-
-
+//                     ) : (
+//                         <div>No bookings found</div>
+//                     )}
 //                 </div>
 
 //             </div>
 //         </div>
-//     )
-// }
+//     );
+// };
 
 // export default CustomerDetails;
+
 
 import React, { useEffect, useState } from 'react';
 import { IoPersonSharp } from 'react-icons/io5';
@@ -148,7 +168,10 @@ const CustomerDetails = () => {
         return resData;
     };
 
-    const generatePDFContent = () => {
+    const generatePDFContent = (bookingId) => {
+        const selectedBooking = booking.find(data => data._id === bookingId);
+        if (!selectedBooking) return;
+
         const doc = new jsPDF();
         doc.setFontSize(22);
         doc.setFont('helvetica', 'bold');
@@ -159,24 +182,19 @@ const CustomerDetails = () => {
         doc.text(`Contact Number: ${customer.contact_number}`, 10, 40);
         doc.setFontSize(18);
         doc.setFont('helvetica', 'bold');
-        doc.text('Bookings:', 10, 60);
-        let y = 70;
-        booking.filter((data) => !data.is_deleted).forEach((data) => {
-            doc.setFontSize(14);
-            doc.setFont('helvetica', 'normal');
-            doc.text(`Movie: ${data.movieId.name}`, 10, y);
-            doc.text(`Theater: ${data.theaterid.name}`, 10, y + 10);
-            doc.text(`Seats: ${data.seats.join(', ')}`, 10, y + 20);
-            doc.text(`Date: ${data.date}`, 10, y + 30);
-            y += 50;
-        });
+        doc.text('Booking Details:', 10, 60);
+        doc.setFontSize(14);
+        doc.setFont('helvetica', 'normal');
+        doc.text(`Movie: ${selectedBooking.movieId.name}`, 10, 70);
+        doc.text(`Theater: ${selectedBooking.theaterid.name}`, 10, 80);
+        doc.text(`Seats: ${selectedBooking.seats.join(', ')}`, 10, 90);
+        doc.text(`Date: ${selectedBooking.date}`, 10, 100);
+
         return doc;
     };
 
-
-
-    const downloadPDF = () => {
-        const doc = generatePDFContent();
+    const downloadPDF = (bookingId) => {
+        const doc = generatePDFContent(bookingId);
         doc.save('booking_details.pdf');
     };
 
@@ -229,7 +247,7 @@ const CustomerDetails = () => {
                                                 <td>{data.date}</td>
 
                                                 <td>
-                                                    <div className='customerdetails-delete-icon' style={{ color: '#1e3249', cursor: 'pointer' }} onClick={downloadPDF}>
+                                                    <div className='customerdetails-delete-icon' style={{ color: '#1e3249', cursor: 'pointer' }} onClick={() => downloadPDF(data._id)}>
                                                         <MdDownload />
                                                     </div>
                                                 </td>
